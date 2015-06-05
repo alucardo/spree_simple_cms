4d23248a1f3eb5c3509226afcf35b214f27e4097
Przemyslaw Olesinski <przemek.4mm@gmail.com>
2015-06-05 08:47:22 +0200

    show page on frontend

--- app/controllers/spree/pages_controller.rb
+++ app/controllers/spree/pages_controller.rb
@@ -1,12 +1,13 @@
 class Spree::PagesController < Spree::BaseController
 
   def show
+    @page = Spree::Page.find_by_slug(request.path)
   end
 
   private
 
-  def get_page
-    
-  end
+ def accurate_title
+      @page.meta_title
+ end
 
 end
